80cf65dcacef3d0e327859d4474012a56cc03eff
magicthanos <thanoskyprianos@gmail.com>
2022-04-26 00:25:01 +0300

    Finished the wpm calculation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import time
+from tkinter import font
 
 #main variables
 text_to_write = list('Hello guys!')
@@ -12,15 +13,15 @@ space_count = 0  #the count of spaces
 input_text = []  #the text we input
 on_first_entry = False
 start_timer = 0
-end_timer = 0
+t = time
 
 
-def read_char(entry):
+def read_char(entry, end_timer=0):
 
-    global word_count, space_count, text_box, input_text, text_to_write, on_first_entry, start_timer, end_timer  #global variables
+    global word_count, space_count, text_box, input_text, text_to_write, on_first_entry, start_timer  #global variables
 
     if not on_first_entry:  #start timer
-        start_timer = time.time()
+        start_timer = t.time()
         on_first_entry = True
 
     #' ' is space
@@ -34,8 +35,16 @@ def read_char(entry):
             text_to_write) and input_text[-1] == ' ':
         text_box.config(state='disabled')
         text_box.unbind('<KeyPress>')
-        end_timer = time.time()
-        print(end_timer - start_timer)
+        end_timer = t.time()
+
+        (gross, net) = calculate_wpm(start_timer, end_timer, input_text,
+                                     text_to_write)
+        wpm_label = Label(
+            root,
+            text=
+            f'Gross WPM: {gross} \nNet WPM: {net} \nTime: {end_timer-start_timer}'
+        )
+        wpm_label.grid(row=1, column=0)
         return
 
     if entry == '\x08':  #\x08 is backspace. If backspace is pressed we pop the last item of the list(if there is one)
@@ -54,7 +63,20 @@ def read_char(entry):
     print(input_text)
 
 
-text_box.grid(row=0, column=0)
+def calculate_wpm(start_time, end_time, input_text, text_to_write):
+    time = (end_time - start_time) / 60
+    gross_wpm = (len(input_text) / 5) / time
+    net_wpm = gross_wpm - (
+        len([c for c in text_to_write if c not in input_text]) / 5) / time
+    return (gross_wpm, net_wpm)
+
+
+text_box.grid(row=2, column=0)
 text_box.bind('<KeyPress>', lambda e: read_char(e.char))
+
+text_display = Label(root,
+                     text=''.join(text_to_write),
+                     font=('JetBrains Mono', 10))
+text_display.grid(row=0, column=0)
 
 mainloop()
